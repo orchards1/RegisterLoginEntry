5e8b69775577d8ab55f76ee0b5f8dd08a25a7352
orchards1 <50650978+orchards1@users.noreply.github.com>
2022-08-01 09:19:37 +0700

    Initial Commit

---
+++ PrivyEntryTests/Register/Controller/RegisterViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RegisterViewController.swift
+//  PrivyEntryTests
+//
+//  Created by Michael Louis on 01/08/22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    var regUrl = "http://pretest-qa.dcidev.id/api/v1/register"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
